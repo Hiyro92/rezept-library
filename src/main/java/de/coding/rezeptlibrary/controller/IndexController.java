db9e77428c0af261f3a5062abf28306cb0e93ee2
Adrian <hiyro92@gmail.com>
2018-11-21 21:16:12 +0100

    Verbindung zum Datenbankserver eingerichtet

---
+++ src/main/java/de/coding/rezeptlibrary/controller/IndexController.java
@@ -0,0 +1,13 @@
+package de.coding.rezeptlibrary.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+
+    @RequestMapping("/")
+    public String test(){
+        return "Hallo";
+    }
+}
