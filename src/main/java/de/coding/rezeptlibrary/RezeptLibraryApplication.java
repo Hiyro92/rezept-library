b8c6e75be83ec01a096fa9f57096b26bf5a952b1
Adrian <hiyro92@gmail.com>
2018-11-21 19:55:42 +0100

    Initial commit

---
+++ src/main/java/de/coding/rezeptlibrary/RezeptLibraryApplication.java
@@ -0,0 +1,12 @@
+package de.coding.rezeptlibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RezeptLibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RezeptLibraryApplication.class, args);
+    }
+}
